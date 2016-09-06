d70df7ce355635295c1156c3e277cb26386b065e
Carolina Martes <carolina.martes@gmail.com>
2016-09-06 18:50:20 -0400

    Set up home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations',  }
-  root 'setup#index'
-  get '/setup' => 'setup#index'
+  root 'home#index'
+  get '/home' => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
