f3fc2e36423651064fe8b85602ebb55590651a80
Anthony Duque <duquea95@gmail.com>
2016-09-07 10:52:11 -0400

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,14 @@
 class HomeController < ApplicationController
+    def index
+
+    end
+
+    def show
+        location = params[:search]
+        limit = params[:limit].to_i
+        params = {limit: limit}
+        if location.present?
+            @responses = Yelp.client.search(location, params)
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations',  }
   root 'home#index'
-  get '/home' => 'home#index'
+  get '/home' => 'home#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
