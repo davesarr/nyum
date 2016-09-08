87e67a9f097cde468a5a8ea6488c5f53177921d5
Carolina Martes <carolina.martes@gmail.com>
2016-09-08 10:19:04 -0400

    Change restaurant-entry non-repeatable

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,24 +12,25 @@ class RestaurantsController < ApplicationController
     if location.present?
         @responses = Yelp.client.search(location, params)
         @responses.businesses.each do |response|
-          Restaurant.create(yelp_id: response.id, name: response.name)
+          if Restaurant.find_by_yelp_id(response.id)==nil
+            then Restaurant.create(yelp_id: response.id, name: response.name)
         end
+      end
     end
-
   end
 
 
   def upvote
   @restaurant = Restaurant.find_by_yelp_id(params[:id])
   @restaurant.upvote_by current_user
-  @likes=@restaurant.get_upvotes.size
+
   redirect_to :back
 end
 
 def downvote
   @restaurant = Restaurant.find_by_yelp_id(params[:id])
   @restaurant.downvote_by current_user
-  @dislikes=@restaurant.get_downvotes.size
+
   redirect_to :back
 end
 
