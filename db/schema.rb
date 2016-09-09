0fbdf8991e7cf8f4a4d4e5869afdbb42d9b30e6c
Carolina Martes <carolina.martes@gmail.com>
2016-09-09 15:40:52 -0400

    Merge resto shows

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908002917) do
+ActiveRecord::Schema.define(version: 20160909160700) do
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20160908002917) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "yelp_id"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "users", force: :cascade do |t|
